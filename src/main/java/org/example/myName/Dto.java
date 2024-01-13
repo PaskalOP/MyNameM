291b9794a02ce1486d8874699c8342a40311f468
PaskalOP <olgapetrovnapaskal@gmail.com>
2024-01-13 13:51:00 +0200

    maven

---
+++ src/main/java/org/example/myName/Dto.java
@@ -0,0 +1,10 @@
+package org.example.myName;
+
+public class Dto {
+    private String name;
+    private String lastName;
+   public Dto (String name, String lastName){
+       this.name=name;
+       this.lastName=lastName;
+   }
+}
---
+++ src/main/java/org/example/myName/Main.java
@@ -0,0 +1,12 @@
+package org.example.myName;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Dto myName = new Dto("Olha","Paskal");
+        //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //gson.toJson(myName)
+        System.out.println("Бла бла бла");
+    }
+}
